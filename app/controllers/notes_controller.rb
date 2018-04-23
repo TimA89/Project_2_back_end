2f6ed221c30988740be9d6d135884585e21bf8cd
TimA89 <timabuladze@gmail.com>
2018-04-23 10:32:35 -0400

    modified notes_controller
    added some methods as index curent user notes

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class NotesController < ApplicationController
+class NotesController < OpenReadController
   before_action :set_note, only: %i[show update destroy]
 
   # GET /notes
@@ -10,14 +10,21 @@ class NotesController < ApplicationController
     render json: @notes
   end
 
+  def index_my_notes
+    @notes = current_user.notes.all
+
+    render json: @notes
+  end
+
   # GET /notes/1
   def show
+    # note = Note.find(params[:id])
     render json: @note
   end
 
   # POST /notes
   def create
-    @note = Note.new(note_params)
+    @note = current_user.notes.build(note_params)
 
     if @note.save
       render json: @note, status: :created, location: @note
@@ -38,17 +45,19 @@ class NotesController < ApplicationController
   # DELETE /notes/1
   def destroy
     @note.destroy
+
+    head :no_content
   end
 
   private
 
   # Use callbacks to share common setup or constraints between actions.
   def set_note
-    @note = Note.find(params[:id])
+    @note = current_user.notes.find(params[:id])
   end
 
   # Only allow a trusted parameter "white list" through.
   def note_params
-    params.require(:note).permit(:title, :date, :note_content)
+    params.require(:note).permit(:title, :date, :note_content, :user_id)
   end
 end
